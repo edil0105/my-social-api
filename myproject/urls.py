1ece77d441b65274a6754073bd869a7cc74733d7
Edil <edil0505t@gmail.com>
2026-03-28 09:48:16 +0500

    Fixed urls and added fix-admin function

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')), # 'myapp' ішіндегі url-дарды қосу
+    path('', include('myapp.urls')), # ОСЫ ЖОЛ МІНДЕТТІ ТҮРДЕ БОЛУЫ КЕРЕК
 ]
